23137f5022d68893f331492fd3fcd673ec495a9d
Kyaw Zin Tun <kyawzintun@kyawzintun.local>
2017-11-11 15:46:08 +0630

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Free Code Camp - Request Header Parser Microservice API
+
+## Getting started
+
+Once you clone the repo you can run:
+
+```
+$ npm install
+$ npm start
+```
+
+App should now be running on [localhost:5000](http://localhost:5000/timestamp-ms/123).
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const useragent = require('useragent');
+useragent(true);
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/req-header-parser', (req, res) => {
+  let header = req.headers;
+  let agent = useragent.parse(header['user-agent']);
+  let obj = {
+    "ipaddress": header.host,
+    "language": getParsedAcceptLangs(header["accept-language"]),
+    "software": agent.os.toString()
+  };
+  res.send(obj);
+});
+
+function getParsedAcceptLangs(hdr) {
+  var pairs = hdr.split(',');
+  for (var i = 0; i < pairs.length; i++) {
+    var pair = pairs[i].split(';');
+    if (pair.length == 1) return pair[0];
+  }
+}
+
+app.listen(port, function () {
+  console.log('Node app is running on port', port);
+});
